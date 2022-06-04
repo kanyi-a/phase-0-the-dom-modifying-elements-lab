7183135f2f644dca61c0851f465ca12366c6eb0a
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-06-04 22:31:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// Write your code here!main.remove();
+// Write your code here!
+main.remove();
 let newHeader = document.createElement("h1");
 newHeader.setAttribute("id","victory");
 newHeader.innerHTML = "YOUR - NAME is the champion";
